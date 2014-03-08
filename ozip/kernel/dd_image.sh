ce11d5f9a14ff4386eaaf12a8441939a1cf7652e
savoca <adeddo27@gmail.com>
2014-03-08 00:36:28 -0500

    Edit script and anykernel script

--- ozip/kernel/dd_image.sh
+++ ozip/kernel/dd_image.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 dd if=/dev/block/platform/msm_sdcc.1/by-name/boot of=/tmp/boot.img
 ./tmp/unpackbootimg -i /tmp/boot.img -o /tmp
-./tmp/mkbootimg --kernel /tmp/zImage-dtb --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 msm_watchdog_v2.enable=1" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 -o /tmp/newboot.img
+./tmp/mkbootimg --kernel /tmp/zImage-dtb --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "$(cat /tmp/boot.img-cmdline)" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 -o /tmp/newboot.img
 dd if=/tmp/newboot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot
 return $?
